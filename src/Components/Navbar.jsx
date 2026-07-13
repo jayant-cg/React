2f51a65fc933b5f06bde14d3ab97eca75214be13
Jayant <jayant.mishra@cginfinity.com>
2026-07-13 10:36:33 +0530

    change navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,10 +3,25 @@ import { NavLink } from 'react-router-dom'
 function Navbar() {
   return (
     <nav className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+      <div className="logo">⚛️ React Profile</div>
+
+      <div className="nav-links">
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+
+        <NavLink to="/profile">
+          Profile
+        </NavLink>
+
+        <NavLink to="/about">
+          About
+        </NavLink>
+
+        <NavLink to="/contact">
+          Contact
+        </NavLink>
+      </div>
     </nav>
   )
 }
